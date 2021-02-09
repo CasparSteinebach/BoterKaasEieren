f83947733ebe24d738c84a4a2290885348bd0092
Caspar Steinebach <casparsteinebach@gmail.com>
2021-02-09 15:56:25 +0100

    winnaar krijgt een vbericht en het spel draait

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,29 @@
 console.log("Hij doet het!!")
 
 /*----- constants -----*/
-let board
-    /*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
+let win;
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+/*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#speelbord div'));
+
+const messages = document.querySelector('h2');
 console.log(squares);
 /*----- event listeners -----*/
-
+document.getElementById('speelbord').addEventListener('click', handleTurn);
 /*----- functions -----*/
 
 function init() {
@@ -27,4 +42,41 @@ function render() {
         squares[index].textContent = mark;
         console.log(mark, squares[index]);
     });
+
+    if (turn === 'X') {
+        messages.textContent = win ? `${win} heeft gewonnen!` : `Het is de beurt aan het ${turn}-je!`;
+    } else {
+        messages.textContent = win ? `${win} heeft gewonnen!` : `Het is de beurt aan het ${turn}-tje!`;
+    }
+
+}
+
+function handleTurn(event) {
+    console.log("hij klikt")
+    let idx =
+        squares.findIndex(function(square) {
+            return square === event.target;
+        });
+
+    board[idx] = turn
+    console.log(board)
+    turn = turn === 'X' ? 'O' : 'X';
+
+    win = getWinner();
+    console.log(win);
+    render();
+
+};
+
+function getWinner() {
+    let winner = null;
+
+    winningCombinations.forEach(function(combination, index) {
+        if (board[combination[0]] && board[combination[0]] === board[combination[1]] &&
+            board[combination[0]] === board[combination[2]])
+            winner = board[combination[0]];
+    });
+    console.log(winner);
+    return winner;
+
 }
